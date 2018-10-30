3e64b61d137c3cf0e0ff158d3aaddbd702b9d71b
Ben Mangold <benmangold@gmail.com>
2018-10-30 12:00:50 -0400

    installing node modules for each repo

--- setup.sh
+++ setup.sh
@@ -1,14 +1,34 @@
-rm -r src/components
 # may prompt you for 'y' to approve deletion
-mkdir src/components
+rm -r src/components
 
+mkdir src/components
 cd src/components
 
 git clone https://github.com/DeltaDrop/dd-Gallery.git
 
+cd dd-Gallery
+npm install
+# seed db - npm run seed
+cd ..
+
 git clone https://github.com/DeltaDrop/dd-ProductInfo.git
+
+cd dd-ProductInfo
+npm install
+# seed db - npm run seed
+cd ..
 
 git clone https://github.com/DeltaDrop/dd-ReviewsOfAProduct.git
 
+cd dd-ReviewsOfAProduct
+npm install
+# seed db - npm run seed
+cd ..
+
 git clone https://github.com/DeltaDrop/dd-OverviewOfAProduct.git
+
+cd dd-OverviewOfAProduct
+npm install
+# seed db - npm run seed
+cd ..
 
