e6441e23420c353022e3f6b0d63d45d2f95b1a66
Ben Mangold <benmangold@gmail.com>
2018-10-30 11:15:02 -0400

    cloning all repos into src

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
+rm -r src
+
 mkdir src
+
+cd src
+
+git clone https://github.com/DeltaDrop/dd-Gallery.git
+
+git clone https://github.com/DeltaDrop/dd-ProductInfo.git
+
+git clone https://github.com/DeltaDrop/dd-ReviewsOfAProduct.git
+
+git clone https://github.com/DeltaDrop/dd-OverviewOfAProduct.git
+
+cd ..
+
