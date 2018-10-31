b28b903f7090695f9b52f9347534033272ecaaad
Ben Mangold <benmangold@gmail.com>
2018-10-31 19:24:48 -0400

    must serve index.html on all proxy requests

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 8080;
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
