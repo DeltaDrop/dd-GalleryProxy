154dcc890511a0d9489c4547ef91ba1bc87268ab
Ben Mangold <benmangold@gmail.com>
2018-11-01 19:36:08 -0400

    setup script not working right, seems to run scripts in parallel

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
