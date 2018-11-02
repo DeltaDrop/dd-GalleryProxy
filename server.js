57e43c38bfddd15efa0108c11c9e12fab07a3938
Ben Mangold <benmangold@gmail.com>
2018-11-02 16:47:14 -0400

    redirect gallery requests to local proxy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
 const port = process.env.PORT || 8080;
 
 app.use(cors({
@@ -15,9 +16,22 @@ app.use(cors({
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', (req, res) => {
+app.get('/buy/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
+
+app.get('/productImages/:product', (req, res) => {
+  let product = req.url.split('/')[2];
+  axios.get('http://127.0.0.1:3000/productImages/' + product)
+  .then((response) => {
+    console.log(response);
+    res.send(response.data)
+  }).catch(err => {
+    res.status(500).send;
+  })
+})
+
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
