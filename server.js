e9982e22d37f73021db0d5d93ff96ae3208cc641
Ben Mangold <benmangold@gmail.com>
2018-11-02 17:39:59 -0400

    clearing out cmts

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.get('/productImages/:product', (req, res) => {
   let product = req.url.split('/')[2];
   axios.get('http://127.0.0.1:3000/productImages/' + product)
   .then((response) => {
-    console.log(response);
     res.send(response.data)
   }).catch(err => {
     res.status(500).send;
@@ -29,29 +28,21 @@ app.get('/productImages/:product', (req, res) => {
 })
 
 app.get('/buy/:product/overview', (req, res) => {
-  console.log('HERE')
   let product = req.url.split('/')[2];
-  console.log(product)
   axios.get('http://127.0.0.1:3003/buy/' + product + '/overview')
   .then((response) => {
-    console.log(response);
     res.send(response.data)
   }).catch(err => {
-    console.log(err)
     res.status(500).send;
   })
 })
 
 app.get('/api/categories/:product', (req,res) => {
-  console.log('HERE')
   let product = req.url.split('/')[3];
-  console.log(product)
   axios.get('http://127.0.0.1:3001/api/categories/' + product)
   .then((response) => {
-    console.log(response.data);
     res.send(response.data)
   }).catch(err => {
-    console.log(err)
     res.status(500).send;
   })
 })
