1d9904fe1da2922f4a6d0f4bc27b6fb0f5f976f7
Ben Mangold <benmangold@gmail.com>
2018-11-02 17:23:15 -0400

    rendering all components

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ app.use(cors({
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/buy/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
 
 app.get('/productImages/:product', (req, res) => {
   let product = req.url.split('/')[2];
@@ -31,7 +28,43 @@ app.get('/productImages/:product', (req, res) => {
   })
 })
 
+app.get('/buy/:product/overview', (req, res) => {
+  console.log('HERE')
+  let product = req.url.split('/')[2];
+  console.log(product)
+  axios.get('http://127.0.0.1:3003/buy/' + product + '/overview')
+  .then((response) => {
+    console.log(response);
+    res.send(response.data)
+  }).catch(err => {
+    console.log(err)
+    res.status(500).send;
+  })
+})
 
+app.get('/api/categories/:product', (req,res) => {
+  console.log('HERE')
+  let product = req.url.split('/')[3];
+  console.log(product)
+  axios.get('http://127.0.0.1:3001/api/categories/' + product)
+  .then((response) => {
+    console.log(response.data);
+    res.send(response.data)
+  }).catch(err => {
+    console.log(err)
+    res.status(500).send;
+  })
+})
+
+// app.get('/api/:product/reviews', (req,res) => {
+//   console.log('HERE')
+//   let product = req.url.split('/')[3];
+//   console.log(product)
+// })
+
+app.get('/buy/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
